7e015fba1bc3b539b2bb13fd8b1bc258a1d5f010
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-08-13 15:17:47 +0300

    First Project

---
+++ S6/Classes/Point3D.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S6.Classes
+{
+    internal class Point3D : IComparable<Point3D> , ICloneable
+    {
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Point3D()
+        {
+            
+        }
+
+        public Point3D(int x , int y , int z)
+        {
+            X= x;
+            Y= y;
+            Z= z;
+        }
+
+        public Point3D(int x , int y) : this (x,y,0)
+        {
+            
+        }
+
+        public Point3D(int x) : this(x,0)
+        {
+            
+        }
+
+        #endregion
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public int CompareTo(Point3D? other)
+        {
+            int CompreX = this.X.CompareTo(other?.X);
+            return CompreX == 0 ? this.Y.CompareTo(other?.Y) : CompreX;
+
+        }
+
+        public object Clone()
+        {
+            return new Point3D()
+            {
+                X = this.X,
+                Y = this.Y,
+                Z = this.Z
+            };
+        }
+
+        #endregion
+
+
+    }
+}
--- S6/Program.cs
+++ S6/Program.cs
@@ -1,10 +1,105 @@
-﻿namespace S6
+﻿using System.Xml;
+using S6.Classes;
+
+namespace S6
 {
     internal class Program
     {
+        
+        public static void IntializeThePoint(Point3D P)
+        {
+            if (P is not null)
+            {
+                bool IsParse;
+                int X ,Y , Z ;
+                do
+                {
+                    Console.Write("Please Enter The X : ");
+                    IsParse = int.TryParse(Console.ReadLine(), out X);
+
+                } while (!IsParse);
+                P.X = X;
+                do
+                {
+                    Console.Write("Please Enter The Y : ");
+                    IsParse = int.TryParse(Console.ReadLine(), out Y);
+
+                } while (!IsParse);
+                P.Y = Y;
+                do
+                {
+                    Console.Write("Please Enter The Z : ");
+                    IsParse = int.TryParse(Console.ReadLine(), out Z);
+
+                } while (!IsParse);
+                P.Z = Z;
+            }
+
+
+        }
         static void Main(string[] args)
         {
-            
+            #region First Project
+
+            #region Q2
+            //Point3D P = new Point3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+            #endregion
+
+            #region Q3
+            //Point3D P1 = new Point3D();
+            //Point3D P2 = new Point3D();
+
+            //Console.WriteLine("please Intialize Point 1");
+            //IntializeThePoint(P1);
+            //Console.WriteLine("please Intialize Point 2");
+            //IntializeThePoint(P2);
+            //Console.WriteLine(P1);
+            //Console.WriteLine(P2);
+            #endregion
+
+            #region Q4
+            // it work and Compare The Refrence then give not Equal
+            //if (P1 == P2)
+            //    Console.WriteLine("Equal");
+            //else
+            //    Console.WriteLine("Not Equal");
+            #endregion
+
+            #region Q5
+
+            //Point3D[] Points =
+            //{
+            //    new Point3D() { X = 40 , Y = 100 , Z = 30 },
+            //    new Point3D() { X = 40 , Y = 50 , Z = 60 },
+            //    new Point3D() { X = 70 , Y = 80 , Z = 90 }
+
+            //};
+
+
+            //Array.Sort(Points);
+            //foreach(Point3D point in Points)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+            #endregion
+
+            //Point3D point01 = new Point3D(1, 2, 3);
+            //Point3D point02 = new Point3D();
+
+            //Console.WriteLine($"Point01 : {point01}");
+            //Console.WriteLine($"Point01 Hash Code : {point01.GetHashCode()}");
+            //Console.WriteLine($"Point02 : {point02}");
+            //Console.WriteLine($"Point02 Hash Code : {point02.GetHashCode()}");
+            //point02 = (Point3D)point01.Clone();
+            //Console.WriteLine($"Point01 : {point01}");
+            //Console.WriteLine($"Point01 Hash Code : {point01.GetHashCode()}");
+            //Console.WriteLine($"Point02 : {point02}");
+            //Console.WriteLine($"Point02 Hash Code : {point02.GetHashCode()}");
+
+
+            #endregion
         }
     }
 }
